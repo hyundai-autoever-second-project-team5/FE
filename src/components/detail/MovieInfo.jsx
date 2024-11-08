bb62020650bf42426393b13a16512dcbd30ea33b
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-11-08 17:11:27 +0900

    feat: add movie info

--- src/components/detail/MovieInfo.jsx
+++ src/components/detail/MovieInfo.jsx
@@ -1,13 +1,3 @@
-// import React from 'react'
-
-// const MovieInfo = () => {
-//   return (
-//     <div>MovieInfo</div>
-//   )
-// }
-
-// export default MovieInfo
-
 import React from 'react';
 
 const MovieInfo = ({
@@ -18,30 +8,39 @@ const MovieInfo = ({
   genres,
   description,
   posterUrl,
+  intro,
 }) => {
   return (
-    <div className="movie-info flex">
-      <div className="w-1/3">
-        <img src={posterUrl} alt={`${title} 포스터`} className="w-full h-auto" />
+    <div className="flex px-5 py-8 text-white bg-black">
+      <div className="relative w-2/6">
+        <img
+          src={
+            "https://img.sbs.co.kr/newsnet/etv/upload/2023/10/10/30000880790.jpg"
+          }
+          alt={`${title} 포스터`}
+          className="w-full h-auto"
+        />
       </div>
-      <div className="w-2/3 pl-6">
-        <h1 className="text-4xl font-bold mb-4">{title}</h1>
-        <div className="mb-4">
-          <p>
-            <strong>개봉일:</strong> {releaseDate}
-          </p>
-          <p>
-            <strong>평점:</strong> {rating}
-          </p>
-          <p>
-            <strong>러닝타임:</strong> {runtime}
-          </p>
+      <div className="w-2/4 pl-6">
+        <h1 className="text-4xl font-bold mb-4">스파이더맨: 홈커밍{title}</h1>
+        <div className="flex items-center justify-between mb-2 ">
+          <strong>| 개봉일: 2017.07.06</strong> {releaseDate}
+          <strong>| 평점: 4.1</strong> {rating}
+          <strong>| 러닝타임: 133분</strong> {runtime}
         </div>
         <div className="mb-4">
-          <strong>장르:</strong> {genres}
+          <strong>| 장르: 액션/SF</strong> {genres}
         </div>
         <div>
-          <strong>상세 정보:</strong>
+          <strong>마블표 스파이더맨, 어벤져스에 합류하다? 스파이더맨 X 아이언맨 역대급 꿀케미를 기대하라!</strong>{intro}
+        </div>
+        <div>
+            {/* 상세정보 */}
+            아이언맨 토니 스타크에게 발탁되어 시빌 워에서 활약했던
+            스파이더맨 피터 파커는 어벤져스에 들어가고 싶은 혈기 넘치고 정의감
+            가득한 10대이다. 그는 진정한 히어로로 거듭나 자신을 증명해
+            보이겠다는 욕심에 위험한 일을 하지 말라는 토니 스타크의 충고를
+            어기고 강력한 적 벌쳐에게 맞선다.
           <p>{description}</p>
         </div>
       </div>
