fc1eadb33ea6ce2c89dcf8d19f3d1ead93c18137
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-11-13 18:47:00 +0900

    Update Detail.jsx

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,14 +6,11 @@ import Footer from "../components/common/Footer";
 
 const Detail = () => {
   return (
-    <div>
       <div className="relative w-full max-w-[1400px] m-auto px-5 py-20 z-20">
         <Info />
         <SwiperCommentList title={"리뷰"} />
         <SwiperCardList title={"추천 영화"} />
       </div>
-      <Footer />
-    </div>
   );
 };
 
