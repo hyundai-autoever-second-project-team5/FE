1c26284adab5446a2497401b53ba238344c84de0
이효원 <ymj07168@gmail.com>
2024-11-13 11:49:27 +0900

    feat: 상세페이지 리뷰 모달 추가

--- src/components/detail/MovieInfo.jsx
+++ src/components/detail/MovieInfo.jsx
@@ -3,17 +3,18 @@ import { useParams } from "react-router-dom";
 import { Avatar, IconButton, Tooltip, Typography } from "@mui/material";
 import ScoreChart from "./ScoreChart";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowUpFromBracket,
-  faHeart,
-  faPen,
-} from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faPen } from "@fortawesome/free-solid-svg-icons";
 import ShareIconButton from "../common/ShareIconButton";
+import ReviewModal from "../common/ReviewModal";
 
 const MovieInfo = () => {
   const { id: movieId } = useParams();
   const [movie, setMovie] = useState(null);
   const [cast, setCast] = useState([]);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const data = [
     { name: "1점", count: 30 },
@@ -52,83 +53,91 @@ const MovieInfo = () => {
   }
 
   return (
-    <div className="py-8 mt-4 text-gray-200">
-      <div className="flex flex-col items-start justify-start sm:flex-row">
-        {/* 영화 포스터 */}
-        <div className="relative w-full sm:max-w-[180px] md:max-w-[250px] lg:max-w-[350px] overflow-hidden">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={`${movie.title} 포스터`}
-            className="w-full"
-          />
-          <div className="absolute inset-0 w-full bg-gradient-to-l from-black to-transparent"></div>
-        </div>
-        {/* 영화 정보 */}
-        <div className="w-full mt-3 sm:mt-0 sm:pl-8">
-          <div className="flex flex-row justify-between w-full items-center">
-            <Typography variant="h2" fontWeight={800}>
-              {movie.title}
-            </Typography>
-            <div className="flex flex-row gap-1">
-              <Tooltip title="리뷰작성" arrow>
-                <IconButton color="inherit">
-                  <FontAwesomeIcon icon={faPen} style={{ fontSize: "24px" }} />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="찜" arrow>
-                <IconButton color="inherit">
-                  <FontAwesomeIcon
-                    icon={faHeart}
-                    style={{ fontSize: "24px" }}
-                  />
-                </IconButton>
-              </Tooltip>
-              <ShareIconButton />
+    <>
+      <div className="py-8 mt-4 text-gray-200">
+        <div className="flex flex-col items-start justify-start sm:flex-row">
+          {/* 영화 포스터 */}
+          <div className="relative w-full sm:max-w-[180px] md:max-w-[250px] lg:max-w-[350px] overflow-hidden">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={`${movie.title} 포스터`}
+              className="w-full"
+            />
+            <div className="absolute inset-0 w-full bg-gradient-to-l from-black to-transparent"></div>
+          </div>
+          {/* 영화 정보 */}
+          <div className="w-full mt-3 sm:mt-0 sm:pl-8">
+            <div className="flex flex-row justify-between w-full items-center">
+              <Typography variant="h2" fontWeight={800}>
+                {movie.title}
+              </Typography>
+              <div className="flex flex-row gap-1">
+                <Tooltip title="리뷰작성" arrow onClick={handleOpen}>
+                  <IconButton color="inherit">
+                    <FontAwesomeIcon
+                      icon={faPen}
+                      style={{ fontSize: "24px" }}
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="찜" arrow>
+                  <IconButton color="inherit">
+                    <FontAwesomeIcon
+                      icon={faHeart}
+                      style={{ fontSize: "24px" }}
+                    />
+                  </IconButton>
+                </Tooltip>
+                <ShareIconButton />
+              </div>
+            </div>
+            <div className="flex flex-row gap-8 sm:gap-8 md:gap-16 mb-2">
+              <strong>| 개봉일: {movie.release_date || "정보 없음"}</strong>
+              <strong>| 평점: {movie.vote_average || "정보 없음"}</strong>
+              <strong>
+                | 러닝타임: {movie.runtime ? `${movie.runtime}분` : "정보 없음"}
+              </strong>
+            </div>
+            <div className="mb-4">
+              <strong>| 장르: </strong>
+              {movie.genres && movie.genres.length > 0
+                ? movie.genres.map((genre) => genre.name).join("/ ")
+                : "정보 없음"}
+            </div>
+            <div className="mb-2 text-2xl font-extrabold">{movie.tagline}</div>
+            <p className="mb-4">
+              {movie.overview || "줄거리 정보가 없습니다."}
+            </p>
+            <div className="w-full h-[300px] lg:max-w-[700px] lg:h-[400px]">
+              <ScoreChart data={data} />
             </div>
           </div>
-          <div className="flex flex-row gap-8 sm:gap-8 md:gap-16 mb-2">
-            <strong>| 개봉일: {movie.release_date || "정보 없음"}</strong>
-            <strong>| 평점: {movie.vote_average || "정보 없음"}</strong>
-            <strong>
-              | 러닝타임: {movie.runtime ? `${movie.runtime}분` : "정보 없음"}
-            </strong>
-          </div>
-          <div className="mb-4">
-            <strong>| 장르: </strong>
-            {movie.genres && movie.genres.length > 0
-              ? movie.genres.map((genre) => genre.name).join("/ ")
-              : "정보 없음"}
-          </div>
-          <div className="mb-2 text-2xl font-extrabold">{movie.tagline}</div>
-          <p className="mb-4">{movie.overview || "줄거리 정보가 없습니다."}</p>
-          <div className="w-full h-[300px] lg:max-w-[700px] lg:h-[400px]">
-            <ScoreChart data={data} />
-          </div>
+        </div>
+        <strong className="block my-4 text-xl">출연진</strong>
+        <div className="flex items-center overflow-x-auto">
+          {cast?.slice(0, 5).map((actor) => (
+            <div
+              key={actor.cast_id}
+              className="flex flex-col items-center text-center min-w-[120px]"
+            >
+              <div className="flex justify-center w-full">
+                <Avatar
+                  alt={actor.name}
+                  src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
+                  sx={{ width: 70, height: 70 }}
+                  className="mb-2"
+                />
+              </div>
+              <div className="w-full truncate">{actor.name}</div>
+              <div className="text-sm text-gray-400 w-full truncate">
+                {actor.character}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-      <strong className="block my-4 text-xl">출연진</strong>
-      <div className="flex items-center overflow-x-auto">
-        {cast?.slice(0, 5).map((actor) => (
-          <div
-            key={actor.cast_id}
-            className="flex flex-col items-center text-center min-w-[120px]"
-          >
-            <div className="flex justify-center w-full">
-              <Avatar
-                alt={actor.name}
-                src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
-                sx={{ width: 70, height: 70 }}
-                className="mb-2"
-              />
-            </div>
-            <div className="w-full truncate">{actor.name}</div>
-            <div className="text-sm text-gray-400 w-full truncate">
-              {actor.character}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+      <ReviewModal open={open} handleClose={handleClose} />
+    </>
   );
 };
 
