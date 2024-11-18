5ad4ba2a5c7a8bb39eb610adeb8c8678726594e0
inwoo0808 <inwoo0808@gmail.com>
2024-11-18 16:04:55 +0900

    cookie 설정

--- src/api/cookie.js
+++ src/api/cookie.js
@@ -1,28 +1,34 @@
-//패키지 임포트
 import { Cookies } from "react-cookie";
 
 const cookies = new Cookies();
 
-const options = {
-  path: "/",
-  secure: true,
-  sameSite: "None",
-};
-//쿠키에 값을 저장할때
-export const setCookie = (name, value, option) => {
+// 쿠키에 값을 저장할 때
+export const setCookie = (name, value, option = {}) => {
   return new Promise((resolve) => {
-    cookies.set(name, value, { options, ...option });
+    cookies.set(name, value, {
+      path: "/",
+      secure: true,
+      sameSite: "None",
+      ...option,
+    });
     resolve();
   });
 };
-//쿠키에 있는 값을 꺼낼때
+
+// 쿠키에 있는 값을 꺼낼 때
 export const getCookie = (name) => {
   return cookies.get(name);
 };
-//쿠키를 지울때
-export const removeCookie = (name) => {
+
+// 쿠키를 지울 때
+export const removeCookie = (name, option = {}) => {
   return new Promise((resolve) => {
-    cookies.remove(name);
+    cookies.remove(name, {
+      path: "/",
+      secure: true,
+      sameSite: "None",
+      ...option,
+    });
     resolve();
   });
 };
