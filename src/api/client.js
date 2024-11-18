5d285a1421fcce7d08861dd1c8f93469740e4de3
inwoo0808 <inwoo0808@gmail.com>
2024-11-18 15:45:18 +0900

    cliend.js 재설정

--- src/api/client.js
+++ src/api/client.js
@@ -1,17 +1,25 @@
+// import axios from "axios";
+
+// const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+// const URL = `${PROXY}`;
+
+// const client = axios.create({
+//   baseURL: URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//     // "Access-Control-Allow-Origin": "http://3.38.104.1:8080",
+//     // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+//     "Access-Control-Allow-Credentials": "true",
+//   },
+//   withCredentials: true,
+// });
+
+// export default client;
 import axios from "axios";
 
-const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
-const URL = `${PROXY}`;
-
 const client = axios.create({
-  baseURL: URL,
-  headers: {
-    "Content-Type": "application/json",
-    // "Access-Control-Allow-Origin": "http://3.38.104.1:8080",
-    // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    "Access-Control-Allow-Credentials": "true",
-  },
-  withCredentials: true,
+  baseURL: "https://api.cinewall.shop",
+  withCredentials: true, // 쿠키를 포함한 요청을 위해 추가
 });
 
 export default client;
