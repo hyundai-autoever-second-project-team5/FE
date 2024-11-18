4cd24d27ea61960b29c26a93c6625d12879a2fdc
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-11-18 15:48:13 +0900

    제발 ....

--- src/api/client.js
+++ src/api/client.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.cinewall.shop";
+// 현재 환경에 따라 baseURL 설정
+
+const BASE_URL =
+  window.location.hostname === "localhost" ? ""
+    : "https://api.cinewall.shop";
 
 const client = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    // "Access-Control-Allow-Origin": "http://3.38.104.1:8080",
-    // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    "Access-Control-Allow-Credentials": "true",
+    // 여기에서 필요한 추가 헤더를 설정할 수 있습니다.
   },
-  withCredentials: true,
+  withCredentials: true, // 쿠키 및 인증 정보 전송을 위해 설정
 });
 
 export default client;
+
 
