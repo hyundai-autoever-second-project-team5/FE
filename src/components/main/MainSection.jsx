cad358b04b8cd267b13a7f21f0131f752f0aecad
이효원 <ymj07168@gmail.com>
2024-11-19 01:02:22 +0900

    feat: 비디오 테스트

--- src/components/main/MainSection.jsx
+++ src/components/main/MainSection.jsx
@@ -24,10 +24,13 @@ const MainSection = ({ videoUrl }) => {
         allowFullScreen
         title="background video"
       /> */}
-      <img
-        src="https://www.10wallpaper.com/wallpaper/1366x768/1107/America_Science_Fiction_Classic_Movie_-_Avatar_HD_Wallpaper_26_1366x768.jpg"
+      <video
+        src="/videos/section3.mp4"
         alt="poster"
         className="absolute object-cover w-full h-full mask-image-gradient"
+        autoPlay
+        muted
+        loop
       />
 
       <div className="absolute inset-0 bg-black opacity-50 mask-image-gradient"></div>
@@ -35,14 +38,14 @@ const MainSection = ({ videoUrl }) => {
       {/* 실제 콘텐츠 */}
       <div className="relative z-10 px-5 m-auto max-w-[1400px] flex flex-col justify-center h-full">
         <div className="flex flex-col gap-3 mb-5">
-          <Typography variant="h1" fontWeight={900} color="white">
+          <div className="text-6xl md:text-7xl lg:text-8xl font-bold text-white">
             아바타
-          </Typography>
-          <Typography variant="h5" color="white" sx={{ maxWidth: "580px" }}>
+          </div>
+          <div className="text-lg md:text-xl lg:text-2xl text-white max-w-[580px]">
             무자비한 용병들이 갑작스러운 공격을 가하며 백안관을 점거한다.
             삽시간에 대혼란이 빚어진 상황. 현장에 있던 한 국회 경찰이 딸을
             구하고 대통령의 안전을 지키기 위해 위험 속으로 뛰어든다.
-          </Typography>
+          </div>
         </div>
         <div className="flex flex-row gap-2">
           <Button
